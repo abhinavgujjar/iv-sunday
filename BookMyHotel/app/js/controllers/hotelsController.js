5a476cd045594af939bcd67a816af792d9fea92c
abhinav <abhinavgujjar@gmail.com>
2014-02-24 12:49:17 +0530

    hotel provider service

--- BookMyHotel/app/js/controllers/hotelsController.js
+++ BookMyHotel/app/js/controllers/hotelsController.js
@@ -1,4 +1,4 @@
-bookMyHotelApp.controller('hotelsController', function($scope){
+bookMyHotelApp.controller('hotelsController', function($scope, hotelsProvider){
 
 	$scope.upVote = function(hotel){
 		hotel.rating ++;
@@ -9,34 +9,5 @@ bookMyHotelApp.controller('hotelsController', function($scope){
 	}
 
 
-	$scope.hotels = [
-	{
-		name : 'Radisson',
-		location : 'Sector 18',
-		image: 'img/radisson.jpg',
-		rating: 8,
-		dailyRate : 6000
-	},
-	{
-		name : 'Hilton',
-		location : 'New Delhi',
-		image: 'img/taj.jpg',
-		rating: 6,
-		dailyRate : 500
-	},
-	{
-		name : 'Hyatt',
-		location : 'New Delhi',
-		image: 'img/hyatt.jpg',
-		rating: 9,
-		dailyRate : 200
-	},
-	{
-		name : 'Mariott',
-		location : 'Gurgaon',
-		image: 'img/royalorchid.jpg',
-		rating: 3,
-		dailyRate : 600
-	}
-	];
+	$scope.hotels = hotelsProvider.hotelsList;
 });
---
+++ BookMyHotel/app/js/services/hotelDataProvider.js
@@ -0,0 +1,35 @@
+bookMyHotelApp.factory('hotelsProvider', function(){
+	return {
+		hotelsList : [
+		{
+			name : 'Radisson',
+			location : 'Sector 18',
+			image: 'img/radisson.jpg',
+			rating: 8,
+			dailyRate : 6000
+		},
+		{
+			name : 'Hilton',
+			location : 'New Delhi',
+			image: 'img/taj.jpg',
+			rating: 6,
+			dailyRate : 500
+		},
+		{
+			name : 'Hyatt',
+			location : 'New Delhi',
+			image: 'img/hyatt.jpg',
+			rating: 9,
+			dailyRate : 200
+		},
+		{
+			name : 'Mariott',
+			location : 'Gurgaon',
+			image: 'img/royalorchid.jpg',
+			rating: 3,
+			dailyRate : 600
+		}
+		]
+	};
+
+});
