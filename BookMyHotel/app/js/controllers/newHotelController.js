884943a845788214ce603e891362f28e101262d5
abhinav <abhinavgujjar@gmail.com>
2014-02-24 17:20:50 +0530

    validation

--- BookMyHotel/app/js/controllers/newHotelController.js
+++ BookMyHotel/app/js/controllers/newHotelController.js
@@ -1,3 +1,14 @@
 bookMyHotelApp.controller('newHotelController', function($scope){
+	$scope.save = function(newHotelForm){
+	console.log(newHotelForm);
+		if ( newHotelForm.$valid)
+		{
+			alert('Valid');
+		}
+		else
+		{
+			alert('not valid');
+		}
+	}	
 
 });
