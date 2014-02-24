39960dbd166f414cba9057e265184e360081a8fc
abhinav <abhinavgujjar@gmail.com>
2014-02-24 18:42:06 +0530

    custom directive

--- BookMyHotel/app/js/directives.js
+++ BookMyHotel/app/js/directives.js
@@ -1,11 +1,12 @@
 'use strict';
 
 /* Directives */
-
-
-angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+bookMyHotelApp.directive('ticks', function(){
+	return {
+		restrict: 'E',
+		template : "<div ng-repeat='a in items' ><span class='glyphicon glyphicon-ok'></span><span class='label label-default' >{{a}}</span></div>",
+		scope:{
+			items: "="
+		}
+	}
+});	
